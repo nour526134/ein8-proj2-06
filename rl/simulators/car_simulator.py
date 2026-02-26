c24d8b4b8a81ed48f56f6daf386c38260e724efa
manalelabboudi <manal@manal-Precision-3561>
2026-02-26 14:25:54 +0100

    correction

--- rl/simulators/car_simulator.py
+++ rl/simulators/car_simulator.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
-from src.gtfs_service import load_stop 
+from src.gtfs_service import load_stops 
 
 class CarSimulator:
     """
