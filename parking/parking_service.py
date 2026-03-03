b643f811e5efdc5e10e0c8f065476f7f60a3f840
Fatima-Zahra SALHI <Fatima-Zahra.Salhi@bordeaux-inp.fr>
2026-03-03 20:53:43 +0100

    added the constructor gtfs in the parking service

--- parking/parking_service.py
+++ parking/parking_service.py
@@ -2,10 +2,13 @@
 import json
 import time
 from pathlib import Path
-from typing import List, Dict, Any, Optional
-
+from typing import Dict, Any, Optional
 import numpy as np
 from parking.osrm_client import OSRMClient
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+from src.gtfs_service import GTFSService
 import pandas as pd
 
 def haversine_m(lat1, lon1, lat2, lon2) -> float:
@@ -30,17 +33,7 @@ def get_parkings(data_path: str = "data/osm/parkings.csv"):
     df = pd.read_csv(file_path)
     parkings = df.to_dict('records')
     return parkings
-def get_trainstations(data_path: str = "data/osm/stations.csv"):
-    """Charge les parkings depuis un fichier CSV"""
-    base_dir = Path.cwd()
-    file_path = base_dir / data_path
-    
-    if not file_path.exists():
-        return []
-    
-    df = pd.read_csv(file_path)
-    parkings = df.to_dict('records')
-    return parkings
+
 class ParkingServiceOSRM:
     """
     Pour chaque station:
@@ -53,12 +46,13 @@ class ParkingServiceOSRM:
         use_public_osrm: bool = True,
         local_osrm_url: Optional[str] = None,
         cache_dir: str = "data/cache",
-        profile: str = "walking",     # si problème: "foot"
+        profile: str = "walking",    
         rate_limit_s: float = 0.3,
         precompute: bool = True,
     ):
         self.parkings = get_parkings()
-        self.stations = get_trainstations()
+        service=GTFSService()
+        self.stations = service.load_stops()
 
         # cache dir
         if not Path(cache_dir).is_absolute():
